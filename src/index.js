05c7344ba67f4f856be935c934cbd29997cd4fde
Kateryna02 <150830031+Kateryna02@users.noreply.github.com>
2024-02-13 17:07:20 +0100

    finished work on my first react project.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import  App  from 'components/App';
 
-// import './index.css';
 
-const Page = () => {
-  return (
-    <div>
-      <App />
-    </div>
-  )
-}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
